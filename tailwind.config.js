949f5a88748e9806ab3269a27d885be0687ce239
Al-Baniah Mukena <mukenaalbaniah@gmail.com>
2022-05-16 16:45:12 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily:{
+      'sans': ['Jost', 'sans-serif'],
+      'serif': ['Source Serif Pro', 'serif']
+    }
+  },
+  plugins: [],
+}
