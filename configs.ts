f6571eac04da6c113a0618202720bb980c56ac4e
Husna Ramadan <husna.ramadan@gmail.com>
2023-06-20 10:33:39 +0700

    Add mukena lunna product link as data

--- configs.ts
+++ configs.ts
@@ -3,5 +3,16 @@ export const LinkTreeData = {
   shopee: "https://shopee.co.id/al_baniah.mukena28",
   lazada: "https://www.lazada.co.id/shop/al-baniah-mukena",
   tiktok: "https://www.tiktok.com/@albaniah_mukena",
-  tokopedia: "https://www.tokopedia.com/albaniahmukena"
-}  
+  tokopedia: "https://www.tokopedia.com/albaniahmukena",
+};
+
+export const LinkTreeLunnaData = {
+  wa: "https://wa.me/6285846513022",
+  shopee:
+    "https://shopee.co.id/Al-Baniah-Mukena-Dewasa-Mini-Traveling-3-in-1-Lunna-Parasit-Korea-Premium-i.558817604.16697507500",
+  lazada:
+    "https://www.lazada.co.id/products/al-baniah-mukena-dewasa-mini-traveling-3-in-1-lunna-parasit-korea-premium-i7502622747-s13918514419.html",
+  tiktok: "https://vt.tiktok.com/ZSLrW1fyR/",
+  tokopedia:
+    "https://www.tokopedia.com/albaniahmukena/al-baniah-mukena-dewasa-mini-traveling-3-in-1-lunna-parasit-korea",
+};
