bac570f890353ea4fa7567d1875bdc706071ec6a
Husna Ramadan <husna.ramadan@gmail.com>
2023-06-28 11:10:45 +0700

    Add arumi resotck header

---
+++ components/arumi-header.tsx
@@ -0,0 +1,12 @@
+export const ArumiHeader = () => {
+  return (
+    <div className="flex flex-col items-center space-y-3 uppercase mb-3">
+      <div className="text-center">
+        <h4 className="text-4xl font-black tracking-wider mb-4">
+          Mukena Arumi Kembali Restock 🎉
+        </h4>
+        <p className="text-lg">Mukena dengan Plisket berbahan sutra velvet.</p>
+      </div>
+    </div>
+  );
+};
